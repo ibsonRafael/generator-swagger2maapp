4cfbb0d4a052381fe16af98bb692ae5da1af0503
ibsonRafael <ibson.rafael@gmail.com>
2018-05-08 11:03:20 -0300

    Angular + ES module generate stub almost done

--- generators/index.js
+++ generators/index.js
@@ -46,7 +46,7 @@ module.exports = Generator.extend({
                 var api = generator.api = swaggerApi;
                 log('API name: %s, Version: %s', api.info.title, api.info.version);
 
-                var tags = [];
+                var tags = generator._tags = [];
                 for (var path in api.paths) {
                     for(var method in api.paths[path]) {
                         // console.log("\t" + method + " tag:" + api.paths[path][method].tags[0]);
@@ -55,8 +55,8 @@ module.exports = Generator.extend({
                         }
                     }
                 }
-                console.log(tags);
-                //log('\t%s sub-contexts candidates found...', tags.length);
+                //console.log(tags);
+                log('\t%s sub-contexts candidates found...\n', tags.length);
 
                 var components = {};
                 for (var tag in tags) {
@@ -92,24 +92,7 @@ module.exports = Generator.extend({
                         api.definitions[definition]['attributes'].push(attributes.charAt(0).toLowerCase() + attributes.slice(1));
                     }
                 }
-                console.log(api.definitions);
-
-
-                generator._tags = tags;
-
-                //Generate files...
-                for (var definition in api.definitions) {
-                    console.log('Creating ' + api.definitions[definition]['modelName'] + '...');
-                }
-
-                for (var component in api.components) {
-                    var componentName = (component.replace(/\.?([A-Z]+)/g, function (x,y){return "-" + y.toLowerCase()}).replace(/^-/, ""))
-                    componentName = componentName + '.component.ts';
-                    console.log('Creating ' + componentName + '...');
-                }
-
-                //git remote add origin https://github.com/user/repo.git
-                git remote -v
+                //console.log(api.definitions);
 
 
                 done();
@@ -124,26 +107,137 @@ module.exports = Generator.extend({
         var swaggerURL = generator.props.swaggerURL;
         var swaggerFilePath = generator.destinationPath('spec/swagger.json');
         var swaggerFileStream;
+        //
+        // try {
+        //     // Make spec folder
+        //     fs.mkdirSync('spec');
+        // } catch (err) {
+        //     log('Folder already exists');
+        // }
+        //
+        // swaggerFileStream = fs.createWriteStream(swaggerFilePath);
+        // // Parsing URL
+        // var url = new URL(swaggerURL);
+        // // Coping original spec from URL
+        // if (url.protocol === 'https:') {
+        //     sync(https.get(swaggerURL).pipe(swaggerFileStream));
+        // } else if (url.protocol === 'http:') {
+        //     sync(http.get(swaggerURL).pipe(swaggerFileStream));
+        // } else {
+        //     // @TODO Add possibility to use a local file using or the path testing if files exists file://
+        //     log('Please provide proper URL (http/https).');
+        // }
 
-        try {
-            // Make spec folder
-            fs.mkdirSync('spec');
-        } catch (err) {
-            log('Folder already exists');
+        //### Generate files - BEGIN ##########################################################################/
+
+        // Creating dockers containers (Dockerfile)
+            // RabbitMQ (Event Bus)
+            // MariaDB|MySql|MSSql|Oracle (Database SQL)
+            // MongoDB (Database NoSQL)
+            // Redis (Cache/Key storing)
+            // Back-End (C#|PHP Lumen|Java Spring API)
+            // Front-End (Angular)
+            // Logstash
+            // Elastic Search
+
+
+        // Creating dockers compuser (yaml)
+
+        // Creating Angular folders
+
+        // Generating models
+        for (var definition in generator.api.definitions) {
+            log('Creating ' + generator.api.definitions[definition]['modelName'] + '...');
         }
 
-        swaggerFileStream = fs.createWriteStream(swaggerFilePath);
-        // Parsing URL
-        var url = new URL(swaggerURL);
-        // Coping original spec from URL
-        if (url.protocol === 'https:') {
-            sync(https.get(swaggerURL).pipe(swaggerFileStream));
-        } else if (url.protocol === 'http:') {
-            sync(http.get(swaggerURL).pipe(swaggerFileStream));
-        } else {
-            // @TODO Add possibility to use a local file using or the path testing if files exists file://
-            log('Please provide proper URL (http/https).');
+        // Generating components
+        for (var component in generator.api.components) {
+            var componentName = (component.replace(/\.?([A-Z]+)/g, function (x,y){return "-" + y.toLowerCase()}).replace(/^-/, ""))
+            componentName = componentName + '.component.ts';
+            log('Creating ' + componentName + '...');
         }
+
+        // Creating actions
+        for(var tag in generator._tags) {
+            log('Creating actions and effects for:' + chalk.red(generator._tags[tag]) + '');
+            for (var path in generator.api.paths) {
+                for (var method in generator.api.paths[path]) {
+                    if (generator._tags[tag] == generator.api.paths[path][method].tags[0]) {
+                        var operation = generator.api.paths[path][method];
+                        var basename = operation.operationId;
+                        log('\tFor operation:' + chalk.red(basename) + '');
+                        basename = (basename.replace(/\.?([A-Z]+)/g, function (x, y) {
+                            return "-" + y.toLowerCase()
+                        }).replace(/^-/, ""));
+
+                        var actionname = basename + '.actions.ts';
+                        var effectname = basename + '.effects.ts';
+                        log('\t\t' + chalk.red(actionname) + '');
+                        log('\t\t' + chalk.red(effectname) + '');
+
+                        actionname = basename + '-done.actions.ts';
+                        effectname = basename + '-done.effects.ts';
+                        log('\t\t' + chalk.red(actionname) + '');
+                        log('\t\t' + chalk.red(effectname) + '');
+
+                        actionname = basename + '-error.actions.ts';
+                        effectname = basename + '-error.effects.ts';
+                        log('\t\t' + chalk.red(actionname) + '');
+                        log('\t\t' + chalk.red(effectname) + '');
+                    }
+                }
+            }
+        }
+
+
+        // Generating module
+        // Generating routing module
+
+        /**
+         * -- > Probed
+         * ** > from config
+         * == > Default config
+         * ++ > from command line
+         * !! > Notice
+         * II > Information
+         * WW > Warning
+         * EE > Error
+         * NI -> Not implemenmted
+         * ?? > Unknow
+         */
+
+        // Creating ES7 Business Module folders
+        // Generating commands classes
+        log(chalk.blue('[es6 module]') + chalk.green('[es6 module]') + 'Creating modules');
+        for(var tag in generator._tags) {
+            var componentname = (generator._tags[tag].replace(/\.?([A-Z]+)/g, function (x,y){return "-" + y.toLowerCase()}).replace(/^-/, ""))
+            componentname = componentname + '.module.js';
+            log('\tCreating module: ' + chalk.yellow(componentname) + '');
+
+            for (var path in generator.api.paths) {
+                for (var method in generator.api.paths[path]) {
+                    if (generator._tags[tag] == generator.api.paths[path][method].tags[0]) {
+                        var operation = generator.api.paths[path][method];
+                        var basename = operation.operationId;
+                        basename = (basename.replace(/\.?([A-Z]+)/g, function (x, y) {
+                            return "-" + y.toLowerCase()
+                        }).replace(/^-/, ""));
+
+                        var commandname = basename + '-command.js';
+                        log('\t\tCreating Command Class ' + chalk.yellow(commandname) + '');
+                    }
+                }
+            }
+        }
+
+        // Generating module
+
+
+        // Creating C# solution folders
+        // Creating C# models
+        // Creating C# controllers
+
+        //### Generate files - END ############################################################################/
 
     //     // Copy files
     //     generator.fs.copy(
