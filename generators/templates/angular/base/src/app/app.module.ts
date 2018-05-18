63fca5f8a66d8fc5362ef496e1d0bbc2e8d799ed
ibsonRafael <ibson.rafael@gmail.com>
2018-05-18 00:48:40 -0300

    time to sleep...

--- generators/templates/angular/base/src/app/app.module.ts
+++ generators/templates/angular/base/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -15,6 +15,9 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppModule { }
